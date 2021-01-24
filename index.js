d02bd538d62cac9d98a302affe7f357c9266d385
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-24 12:48:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,20 +2,15 @@
 
 
 function findMatching (list, name) {
-  return list.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase();
-  });
-}
-
+ return list.filter(function (driverName) {
+ return driverName.toLowerCase() === name.toLowerCase();
+ });}
 function fuzzyMatch (list, partialName) {
-  let lengthOfName = partialName.length;
-  return list.filter(function (driverName) {
-    return driverName.slice(0, lengthOfName) === partialName;
-  });
-}
-
+ let lengthOfName = partialName.length;
+ return list.filter(function (driverName) {
+ return driverName.slice(0, lengthOfName) === partialName;
+  });}
 function matchName (list, name) {
-  return list.filter(function (driver) {
-    return driver.name.toLowerCase() === name.toLowerCase();
-  });
-}
+ return list.filter(function (driver) {
+ return driver.name.toLowerCase() === name.toLowerCase();
+  });}
